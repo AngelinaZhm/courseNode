87e6bffe7a28170e2b12e0f77885d0bfe5191841
Anhelina <zhmurkevych447@gmail.com>
2026-03-30 15:39:17 +0300

    jwt

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,15 +7,17 @@ import { inject, injectable } from 'inversify';
 import { IUserController } from './users.controller.interface';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
-import { User } from './user.entity';
-import { UserService } from './users.service';
 import { ValidateMiddleware } from '../common/validate.middleware';
+import { sign } from 'jsonwebtoken';
+import type { IUserService } from './users.service.interface';
+import type { IConfigService } from '../config/config.service.interface';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
 	constructor(
 		@inject(TYPES.ILogger) private LoggerService: ILogger,
-		@inject(TYPES.UserService) private userService: UserService,
+		@inject(TYPES.UserService) private userService: IUserService,
+		@inject(TYPES.ConfigService) private configService: IConfigService,
 	) {
 		super(LoggerService);
 		this.bindRoutes([
@@ -43,7 +45,8 @@ export class UserController extends BaseController implements IUserController {
 		if (!isValid) {
 			return next(new HTTPError(401, 'помилка авторизації', 'login'));
 		}
-		this.ok(res, 'Успішна авторизація');
+		const jwt = await this.signJWT(req.body.email, this.configService.get('SECRET'));
+		this.ok(res, { jwt });
 	}
 
 	async register(
@@ -56,5 +59,26 @@ export class UserController extends BaseController implements IUserController {
 			return next(new HTTPError(422, 'Такий користувач вже існує'));
 		}
 		this.ok(res, { email: result.email, id: result.id });
+	}
+
+	private signJWT(email: string, secret: string): Promise<string> {
+		return new Promise<string>((resolve, reject) => {
+			sign(
+				{
+					email,
+					iat: Math.floor(Date.now() / 1000),
+				},
+				secret,
+				{
+					algorithm: 'HS256',
+				},
+				(err, token) => {
+					if (err) {
+						reject(err);
+					}
+					resolve(token as string);
+				},
+			);
+		});
 	}
 }
